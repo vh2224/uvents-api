e761ccc73d8cec306de01b2e5870afca376e69fc
Vinicius <vinihenriqueh@gmail.com>
2022-10-10 22:36:42 -0300

    corrigindo deploy

--- dist/utils/trimBeforeAfter.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.trimBeforeAndAfter = void 0;
-const trimBeforeAndAfter = (value) => {
-    return value.trimStart().trimEnd();
-};
-exports.trimBeforeAndAfter = trimBeforeAndAfter;
